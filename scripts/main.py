6ada1d5130aa457ab1a4327c9fee6a871043e56d
madannes <7156191+madannes@users.noreply.github.com>
2021-02-12 10:19:56 -0500

    Removed extraneous settings and tweaked CSS a bit

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,11 @@
 import logging
-import settings
 from weasyprint import HTML
 
-logger = logging.getLogger(settings.STANDARD_LOGGER)
+logger = logging.getLogger('weasyprint_test')
 
 logger.info('Starting up...')
 
-for i in range(0, 1):
+for i in range(0, 3):
     html = HTML('templates/test.html')
     html.write_pdf(f'templates/output/test{i + 1}.pdf')
 
